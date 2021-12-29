f712aeb9626149d161072095ace3c77b5e6ef194
Yajat Vishwak <yajat472001@gmail.com>
2021-12-29 15:36:46 +0530

    bro

--- src/pages/StudentLogin.jsx
+++ src/pages/StudentLogin.jsx
@@ -1,4 +1,21 @@
+import axios from "axios";
+
 function StudentLogin(params) {
+  function onClick(e) {
+    e.preventDefault();
+    console.log("Click ho gaya hai bc");
+    axios
+      .post("http://localhost:5000/loginStudent", {
+        username: username,
+        password: password,
+      })
+      .then((res) => {
+        if (res.data.message === "auth successful") {
+        } else {
+          alert("wrong credentials");
+        }
+      });
+  }
   return (
     <section className="h-screen bg-gray-800 flex flex-col justify-center items-center  ">
       <div className="text-3xl bg-white p-6 rounded-xl ">
@@ -21,6 +38,7 @@ function StudentLogin(params) {
           />
           <button
             type="submit"
+            onClick={(e) => onClick(e)}
             className="bg-yellow-300 text-white text-lg font-bold px-6 py-6 rounded-2xl"
           >
             Login
